601d3fd7e602f4c15baf0c4e6f024468a9d977e2
reghina21 <143565962+reghina21@users.noreply.github.com>
2025-04-15 14:26:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,18 @@ menu = st.sidebar.radio("Pilih Fitur", ["📁 Dataset", "📈 Visualisasi Datase
 # Global Data Container
 if 'df' not in st.session_state:
     st.session_state.df = None
+
+# 1. Dataset
+if menu == "📁 Dataset":
+    st.subheader("Upload Dataset Kurs")
+    uploaded_file = st.file_uploader("Upload file CSV dengan kolom: tanggal, beli_yuan, jual_yuan, beli_dollar, jual_dollar", type=["csv"])
+
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file, parse_dates=["tanggal"])
+        df.sort_values("tanggal", inplace=True)
+        st.session_state.df = df
+        st.success("✅ Dataset berhasil diupload!")
+
+    if st.session_state.df is not None:
+        st.write("📄 Data Kurs:")
+        st.dataframe(st.session_state.df)
